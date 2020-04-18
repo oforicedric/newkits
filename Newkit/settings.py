c92c42e8eddb92ecf2996e46a9a3003b5a845837
cedrick oforii <oforicedric@gmail.com>
2020-04-18 22:02:25 +0100

    2nd try after fiver

--- Newkit/settings.py
+++ Newkit/settings.py
@@ -113,20 +113,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'europe/london'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static/'),
     ]
