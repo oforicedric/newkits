8911c2881b12fc1463e380062d81e7442b98ad7c
cedrick oforii <oforicedric@gmail.com>
2020-04-18 12:47:53 +0100

    3

--- Newkit/settings.py
+++ Newkit/settings.py
@@ -113,13 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+00:00'
+#TIME_ZONE = 'UTC+00:00'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
