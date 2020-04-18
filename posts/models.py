070229b3dca81af1ac8ffddc1fad55cfbfdfb26b
cedrick oforii <oforicedric@gmail.com>
2020-04-18 16:50:41 +0100

    5th

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from data.models import Club
 
 # Create your models here.
 
 class Posts(models.Model):
     name = models.CharField(max_length=100, default="user")
     value = models.CharField(max_length=100)
-
+    team = models.ForeignKey(Club, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.name
